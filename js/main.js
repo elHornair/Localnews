baebd73b4b66e713bf4e181e6d536d911b67ddf3
Alain Horner <alain.horner@liip.ch>
2011-09-22 17:23:41 +0200

    extract js-code into js-file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 YUI().use("io-base", function(Y) {
-    var uri = "data.php";
 
     function complete(id, response) {
         var data = response.responseText;
@@ -7,6 +6,31 @@ YUI().use("io-base", function(Y) {
     };
 
     Y.on('io:complete', complete, Y);
-    var request = Y.io(uri);
+
+    function supports_geolocation() {
+        return !!navigator.geolocation;
+    }
+
+    show_map = function handle_success(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        var speed = position.coords.speed;
+
+        Y.log(latitude, longitude, speed);
+        Y.io("data.php");
+    }
+
+    handle_error = function handle_error(e) {
+        if (e.code == 1) {
+            Y.log("user said no");
+        }
+    }
+
+    if (supports_geolocation()) {
+        navigator.geolocation.getCurrentPosition(handle_success, handle_error);
+    } else {
+        Y.log("no decent browser");
+    };
+
 
 });
