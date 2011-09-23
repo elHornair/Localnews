e30579aa42817b9ed9c7bbd26992c2e539e478e7
Alain Horner <alain.horner@liip.ch>
2011-09-23 23:43:16 +0200

    added a basic transition

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ YUI({
            fullpath: "js/ln-geolocation.js"
        }
     }
-}).use('io-base', 'ln-geolocation', function(Y) {
+}).use('io-base', 'transition', 'ln-geolocation', function(Y) {
 
+    // geolocation
     Y.ln.geolocation.getCoords();
 
+    // ajax
     function complete(id, response) {
         var data = response.responseText;
         Y.log(data);complete
@@ -15,5 +17,15 @@ YUI({
 
     Y.on('io:complete', complete, Y);
     Y.io("data.php");
+
+    // animation
+    Y.one('#pointer').transition({
+        easing: 'ease-out',
+        duration: 0.75,
+        top: '200px',
+        left: '300px'
+    }, function() {
+        Y.log("feddig: "+this);
+    });
 
 });
